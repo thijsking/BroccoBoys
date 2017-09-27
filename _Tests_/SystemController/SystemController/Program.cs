54dd5d968baff6925ca4d8a58dd5887e610ecc9c
SandervanGemmert <Sandervangemmert@gmail.com>
2017-09-27 17:59:10 +0200

    PLC + HMI (Trilgoot added)
    Trilgoot werkt met HMI aan uit knop (als de start knop eenmalig handmatig is ingedrukt

--- _Tests_/SystemController/SystemController/Program.cs
+++ _Tests_/SystemController/SystemController/Program.cs
@@ -13,7 +13,7 @@ namespace SystemController
         {
             string plc_ip = "192.168.125.2";
             var client = new S7Client();
-            int result = client.ConnectTo(plc_ip, 0, 0);
+            int result = client.ConnectTo(plc_ip, 0, 1);
             
             if(result == 0)
             {

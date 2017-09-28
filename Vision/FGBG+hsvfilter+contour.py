bc64a3bd95496410b03f28748452cefe64aff9a9
Stijn2211 <32327368+Stijn2211@users.noreply.github.com>
2017-09-28 15:02:02 +0200

    Laatste versie vision programma

--- Vision/FGBG+hsvfilter+contour.py
+++ Vision/FGBG+hsvfilter+contour.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
 import socket
+from threading import Thread
 
 HOST = '192.168.125.4'		# Symbolic name, meaning all available interfaces
 PORT = 8004  				# Arbitrary non-privileged port
-
+ReadyToWrite = 0
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socket created')
 
@@ -12,7 +13,6 @@ print('Socket created')
 
 sock.bind((HOST, PORT))
 print('Socket now bind')
-
 
 print('Socket bind complete')
 
@@ -22,11 +22,30 @@ print('Socket now listening')
 
 conn, addr = sock.accept()
 
-cap = cv2.VideoCapture(1)
+def WaitForReady():
+    global ReadyToWrite
+    global conn
+    print('entered function')
+    while True:
+        #print('looping')
+        if (ReadyToWrite == 0):
+            #print('aan het lezen')
+            received = conn.recv(256)
+            print (received.decode())
+            if received.decode() == 'ready':
+                ReadyToWrite = 1
+                print ('ready received')
+
+
+cap = cv2.VideoCapture(0)
 cv2.ocl.setUseOpenCL(False)
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
+Threadje = Thread(target=WaitForReady)
+Threadje.start()
+
 while True:
+    #print('in main while')
     _, frame = cap.read()
     frame = frame[150:380,0:450]
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV, 0)
@@ -49,12 +68,14 @@ while True:
 
     _, contours, hierarchy = cv2.findContours(mask   , cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     if contours:
+        #print('contouyrs')
         cnt = contours[0]
         area = cv2.contourArea(cnt)
         if area > 0:
             cv2.drawContours(frame, contours, -1, (0, 255, 255), 3)
             M = cv2.moments(cnt)
-            if area > 50:
+            if area > 500:
+                #print('grote area')
                 #hull = cv2.convexHull(cnt)
                 #epsilon = 0.1 * cv2.arcLength(cnt, True)
                 #approx = cv2.approxPolyDP(cnt, epsilon, True)
@@ -62,7 +83,7 @@ while True:
                 cx = int(M['m10'] / M['m00'])
                 cy = int(M['m01'] / M['m00'])
                 cv2.circle(frame, (cx, cy), 10, (0, 0, 255), -1)
-                print("x=", cx, "y=", cy)
+                #print("x=", cx, "y=", cy)
                 array = [cx, cy]
                 rx = int(cx*-2.1+1017.4)
                 if rx>565:
@@ -70,13 +91,19 @@ while True:
                 if rx<130:
                     rx = 130
 
-        message = bytes(str(rx),'utf8')
-        conn.send(message)
-        message = ''
+                #print('voor check')
+                if(ReadyToWrite == 1):
+                    #print(str(rx))
+                    message = bytes(str(rx),'utf8')
+                    conn.send(message)
+                    message = ''
+                    ReadToWrite = 0
 
     cv2.imshow('Original', frame)
     cv2.imshow('fgbg', mask)
     cv2.imshow('res', res)
+
+
 
     k = cv2.waitKey(30)
     if k == 27:
@@ -84,5 +111,5 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
-def pixelvalue():
-    return rx
+
+

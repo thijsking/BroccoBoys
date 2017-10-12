1e197605244092ce4f3567d0c8d38c86a0138943
Stijn2211 <32327368+Stijn2211@users.noreply.github.com>
2017-10-12 10:00:57 +0200

    Vision X Y en Hoek voor lichtbox

--- Vision/Vision test X en Y.py
+++ Vision/Vision test X en Y.py
@@ -7,34 +7,34 @@ cap = cv2.VideoCapture(0)
 
 while True:
     _, frame = cap.read()
-    frame = frame[235:335,0:450]
+    #frame = frame[235:335,0:450]
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV, 0)
-
-    lower_red_stam = np.array([21, 51, 105])
-    upper_red_stam = np.array([51, 168, 255])
+    hsv = cv2.medianBlur(hsv,11)
+    lower_red_stam = np.array([16, 0, 181])
+    upper_red_stam = np.array([25, 218, 255])
 
     mask_stam = cv2.inRange(hsv, lower_red_stam, upper_red_stam)
     res_stam = cv2.bitwise_and(frame, frame, mask=mask_stam)
 
     kernel = np.ones((5, 5), np.uint8)
     mask_stam = cv2.dilate(mask_stam, kernel, iterations=1)
-    mask_stam = cv2.erode(mask_stam, kernel, iterations=3)
+    mask_stam = cv2.erode(mask_stam, kernel, iterations=8)
     mask_stam = cv2.dilate(mask_stam, kernel, iterations=5)
     #mask = cv2.erode(mask, kernel, iterations=4)
     #mask = cv2.dilate(mask, kernel, iterations=5)
 
-    lower_red_broc = np.array([35, 17, 0])
-    upper_red_broc = np.array([87, 255, 255])
+    lower_red_broc = np.array([0, 140, 0])
+    upper_red_broc = np.array([26, 255, 158])
 
     mask_broc = cv2.inRange(hsv, lower_red_broc, upper_red_broc)
     res_broc = cv2.bitwise_and(frame, frame, mask=mask_broc)
 
     kernel = np.ones((5, 5), np.uint8)
     mask_broc = cv2.dilate(mask_broc, kernel, iterations=1)
-    mask_broc = cv2.erode(mask_broc, kernel, iterations=3)
+    mask_broc = cv2.erode(mask_broc, kernel, iterations=5)
     mask_broc = cv2.dilate(mask_broc, kernel, iterations=2)
-    mask_broc = cv2.erode(mask_broc, kernel, iterations=4)
-    mask_broc = cv2.dilate(mask_broc, kernel, iterations=5)
+    mask_broc = cv2.erode(mask_broc, kernel, iterations=5)
+    mask_broc = cv2.dilate(mask_broc, kernel, iterations=9)
 
     X_stam = 0
     Y_stam = 0
@@ -46,10 +46,10 @@ while True:
     if contours_stam:
         cnt = contours_stam[0]
         area = cv2.contourArea(cnt)
-        if area > 0:
+        if area > 5000:
             cv2.drawContours(frame, contours_stam, -1, (0, 255, 255), 3)
             M = cv2.moments(cnt)
-            if area > 50:
+            if area > 5000:
                 rect = cv2.minAreaRect(cnt)
                 box = cv2.boxPoints(rect)
                 box = np.int0(box)
@@ -62,10 +62,10 @@ while True:
     if contours_broc:
         cnt = contours_broc[0]
         area = cv2.contourArea(cnt)
-        if area > 0:
-            cv2.drawContours(frame, contours_broc, -1, (0, 255, 255), 3)
+        if area > 50000:
+            cv2.drawContours(frame, contours_broc, 0, (255, 0, 255), 3)
             M = cv2.moments(cnt)
-            if area > 700:
+            if area > 50000:
                 X_broc = int(M['m10'] / M['m00'])
                 Y_broc = int(M['m01'] / M['m00'])
                 cv2.circle(frame, (X_broc, Y_broc), 10, (0, 0, 255), -1)
@@ -78,18 +78,19 @@ while True:
                 if rx < 130:
                     rx = 130
 
-    # if(X_stam > X_broc):
-    #     DeltaX = X_stam - X_broc
-    # else:
-    #     DeltaX = X_broc - X_broc
-    # if (Y_stam > Y_broc):
-    #     DeltaY = Y_stam - Y_broc
-    # else:
-    #     DeltaX = Y_broc - Y_broc
-    #
-    # if (DeltaX != 0 and DeltaY != 0):
-    #     Alpha = math.atan(DeltaY/DeltaX)
-    #     print(Alpha)
+
+    if(X_stam > X_broc):
+        DeltaX = X_stam - X_broc
+    else:
+        DeltaX = X_broc - X_stam
+    if (Y_stam > Y_broc):
+        DeltaY = Y_stam - Y_broc
+    else:
+        DeltaX = Y_broc - Y_stam
+
+    if (DeltaX != 0 and DeltaY != 0):
+        Alpha = math.atan(DeltaY/DeltaX)
+        print(Alpha)
 
 
     cv2.imshow('Original', frame)
